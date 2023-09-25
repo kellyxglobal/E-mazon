60c6451e80607662daccc6ffa8f2da6bb35623f1
Kelechi <kellyxglobal@gmail.com>
2023-09-25 17:31:53 +0100

    creating fields to be added to the Database via Seeders directory

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Admin
+        DB::table('users')->insert([
+            //Amin - add oject data
+            [
+                'name' => 'Admin',
+                'username' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'admin',
+                'status' => 'active',
+
+            ],
+            //Vendor - add object data
+            [
+                'name' => 'Kellyx Vendor',
+                'username' => 'vendor',
+                'email' => 'vendor@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'vendor',
+                'status' => 'active',
+
+            ],
+            //User or Customer
+            [
+                'name' => 'User',
+                'username' => 'user',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'user',
+                'status' => 'active',
+
+            ]
+        ])
+
+
+
+
+
+    }
+}
