d5fbe39c6ce35aad76d002eb68db96d0f6922c3c
Kelechi <kellyxglobal@gmail.com>
2023-09-30 10:45:27 +0100

    Loading the admin theme and displaying the admin dashbard theme loaded on admin login from the web.php by copying the asset folder of the backend theme and pasting it into my project public directory

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AdminController extends Controller
 {
     public function AdminDashboard(){
-        return view('admin.admin_dashbaord');
+        return view('admin.index');
+    }//End Method
+
+    public function AdminLogin(){
+        return view('admin.admin_login');
     }
-    //
+
+
+    public function AdminDestroy(Request $request){
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
+    }//End Method
+
+    public function AdminProfile(Request $request){
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+        return view('admin.admin_profile_view', compact('adminData'));
+    }//End Method
+
 }
--- routes/web.php
+++ routes/web.php
@@ -36,9 +36,16 @@ require __DIR__.'/auth.php';
 
 Route::middleware(['auth','role:admin'])->group(function() {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+
+    Route::get('/admin/logout', [AdminController::class, 'AdminDestroy'])->name('admin.logout');
+    
+    Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
 });
 
-//FrontEnd Dashboard
+//Vendor Dashboard
 Route::middleware(['auth','role:vendor'])->group(function() {
     Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])->name('vendor.dashboard');
 });
+
+
+Route::get('/admin/login', [AdminController::class, 'AdminLogin']);
