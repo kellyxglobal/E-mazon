f17fa656aa5dbb379a19d71863711e7f65a37818
Kelechi <kellyxglobal@gmail.com>
2023-10-01 23:25:50 +0100

    updating comments

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -16,7 +16,7 @@ class AdminController extends Controller
         return view('admin.admin_login');
     }
 
-
+    //A function or method to destroy the admin profile session or to logout of the admin profile page.
     public function AdminDestroy(Request $request){
         Auth::guard('web')->logout();
 
@@ -27,12 +27,15 @@ class AdminController extends Controller
         return redirect('/admin/login');
     }//End Method
 
+    //A method to view a specific admin profile page using the id concept since its a unique value among all the fields.
     public function AdminProfile(Request $request){
         $id = Auth::user()->id;
         $adminData = User::find($id);
         return view('admin.admin_profile_view', compact('adminData'));
     }//End Method
 
+
+    //Updating all input fields in the admin profile form into the database
     public function AdminProfileStore(Request $request){
         $id = Auth::user()->id;
         $data = User::find($id);
@@ -41,6 +44,7 @@ class AdminController extends Controller
         $data->phone = $request->phone;
         $data->address = $request->address;
 
+        //Updating admin profile photo and aking sure the new profile photo rplaces the old one  using the laravl @unlink method.
         if($request->file('photo')){
             $file = $request->file('photo');
             @unlink(public_path('upload/admin_images/'.$data->photo));
