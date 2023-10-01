7efb4529ca3753e9b3c4ec5e2655d830fb98796a
Kelechi <kellyxglobal@gmail.com>
2023-10-01 23:16:14 +0100

    adding a method to replace an old uploaded photo with a new one in the admin profile edit page from the AdminController.php file

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -43,7 +43,7 @@ class AdminController extends Controller
 
         if($request->file('photo')){
             $file = $request->file('photo');
-            @unlink(public_path('upload/admin_images/'.$data));
+            @unlink(public_path('upload/admin_images/'.$data->photo));
             $filename = date('YmdHi').$file->getClientOriginalName();
             $file->move(public_path('upload/admin_images'),$filename);
             $data['photo'] = $filename;
