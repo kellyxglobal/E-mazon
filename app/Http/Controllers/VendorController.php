c34b04089bdb6b3f6b8ac7619805a3c011df47b7
Kelechi <kellyxglobal@gmail.com>
2023-10-04 18:01:30 +0100

    Working on Vendors Profile page

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -37,4 +37,44 @@ class VendorController extends Controller
         return view('vendor.vendor_profile_view', compact('vendorData'));
     }//End Method
 
+
+
+
+     //Updating all input fields in the vendor profile form into the database
+     public function VendorProfileStore(Request $request){
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+        $data->vendor_join = $request->vendor_join;
+        $data->vendor_short_info = $request->vendor_short_info;
+
+        //Updating admin profile photo and aking sure the new profile photo rplaces the old one  using the laravl @unlink method.
+        if($request->file('photo')){
+            $file = $request->file('photo');
+            @unlink(public_path('upload/vendor_images/'.$data->photo));
+            $filename = date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/vendor_images'),$filename);
+            $data['photo'] = $filename;
+        }
+
+        $data->save();
+
+        //Adding notification in the admin profile page to display a message when ever the admin update his/her profile bu uisng the toastr function
+        $notification = array(
+            'message' => 'Vendor Profile Updated Succesfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
+
+        return view('vendor.vendor_profile_store_view');
+    }//End Method
+
+    public function VendorChangePassword(Request $request){
+        return view('vendor.vendor_change_password');
+       
+    }//End Method
+
 }
--- resources/views/vendor/body/header.blade.php
+++ resources/views/vendor/body/header.blade.php
@@ -332,7 +332,7 @@
 
 					<div class="user-box dropdown">
 						<a class="d-flex align-items-center nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-							<img src="{{ (!empty($adminData->photo)) ? url('upload/admin_images/'.$adminData->photo):url('upload/no_image.jpg') }}" class="user-img" alt="user avatar">
+							<img src="{{ (!empty($vendorData->photo)) ? url('upload/vendor_images/'.$vendorData->photo):url('upload/no_image.jpg') }}" class="user-img" alt="user avatar">
 							<div class="user-info ps-3">
 								<p class="user-name mb-0">{{ Auth::user()->name }}</p>
 								<p class="designattion mb-0">{{ Auth::user()->username }}</p>
