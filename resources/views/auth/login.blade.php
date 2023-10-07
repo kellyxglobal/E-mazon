70c56b14ca6f7c6eccb92990f7e5244a6459bdc3
Kelechi <kellyxglobal@gmail.com>
2023-10-07 13:52:18 +0100

    Finish customizing user login page as per demand

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,7 +29,7 @@
             <div class="container">
                 <div class="breadcrumb">
                     <a href="index.html" rel="nofollow"><i class="fi-rs-home mr-5"></i>Home</a>
-                    <span></span> Pages <span></span> My Account
+                    <span></span>  <span></span> My Account
                 </div>
             </div>
         </div>
@@ -39,7 +39,7 @@
                     <div class="col-xl-8 col-lg-10 col-md-12 m-auto">
                         <div class="row">
                             <div class="col-lg-6 pr-30 d-none d-lg-block">
-                                <img class="border-radius-15" src="assets/imgs/page/login-1.png" alt="" />
+                                <img class="border-radius-15" src="{{ asset('frontend/assets/imgs/page/login-1.png') }}" alt="" />
                             </div>
                             <div class="col-lg-6 col-md-8">
                                 <div class="login_wrap widget-taber-content background-white">
@@ -48,24 +48,17 @@
                                             <h1 class="mb-5">Login</h1>
                                             <p class="mb-30">Don't have an account? <a href="page-register.html">Create here</a></p>
                                         </div>
-                                        <form method="post">
+
+
+                                        <form method="POST" action="{{ route('login') }}">
+                                            @csrf
                                             <div class="form-group">
-                                                <input type="text" required="" name="email" placeholder="Username or Email *" />
+                                                <input type="email" id="email" required="" name="email" placeholder="Username or Email *" />
                                             </div>
                                             <div class="form-group">
-                                                <input required="" type="password" name="password" placeholder="Your password *" />
+                                                <input required="" type="password" id="password" name="password" placeholder="Your password *" />
                                             </div>
-                                            <div class="login_footer form-group">
-                                                <div class="chek-form">
-                                                    <input type="text" required="" name="email" placeholder="Security code *" />
-                                                </div>
-                                                <span class="security-code">
-                                                    <b class="text-new">8</b>
-                                                    <b class="text-hot">6</b>
-                                                    <b class="text-sale">7</b>
-                                                    <b class="text-best">5</b>
-                                                </span>
-                                            </div>
+                                            
                                             <div class="login_footer form-group mb-50">
                                                 <div class="chek-form">
                                                     <div class="custome-checkbox">
@@ -73,12 +66,14 @@
                                                         <label class="form-check-label" for="exampleCheckbox1"><span>Remember me</span></label>
                                                     </div>
                                                 </div>
-                                                <a class="text-muted" href="#">Forgot password?</a>
+                                                <a class="text-muted" href="{{ route('password.request') }}">Forgot password?</a>
                                             </div>
                                             <div class="form-group">
                                                 <button type="submit" class="btn btn-heading btn-block hover-up" name="login">Log in</button>
                                             </div>
                                         </form>
+
+
                                     </div>
                                 </div>
                             </div>
--- resources/views/frontend/body/footer.blade.php
+++ resources/views/frontend/body/footer.blade.php
@@ -15,7 +15,7 @@
                                     <button class="btn" type="submit">Subscribe</button>
                                 </form>
                             </div>
-                            <img src="assets/imgs/banner/banner-9.png" alt="newsletter" />
+                            <img src="{{ asset('frontend/assets/imgs/banner/banner-9.png') }}" alt="newsletter" />
                         </div>
                     </div>
                 </div>
@@ -27,7 +27,7 @@
                     <div class="col-lg-1-5 col-md-4 col-12 col-sm-6 mb-md-4 mb-xl-0">
                         <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp" data-wow-delay="0">
                             <div class="banner-icon">
-                                <img src="assets/imgs/theme/icons/icon-1.svg" alt="" />
+                                <img src="{{ asset('frontend/assets/imgs/theme/icons/icon-1.svg') }}" alt="" />
                             </div>
                             <div class="banner-text">
                                 <h3 class="icon-box-title">Best prices & offers</h3>
@@ -38,7 +38,7 @@
                     <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
                         <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp" data-wow-delay=".1s">
                             <div class="banner-icon">
-                                <img src="assets/imgs/theme/icons/icon-2.svg" alt="" />
+                                <img src="{{ asset('frontend/assets/imgs/theme/icons/icon-2.svg') }}" alt="" />
                             </div>
                             <div class="banner-text">
                                 <h3 class="icon-box-title">Free delivery</h3>
@@ -49,7 +49,7 @@
                     <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
                         <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp" data-wow-delay=".2s">
                             <div class="banner-icon">
-                                <img src="assets/imgs/theme/icons/icon-3.svg" alt="" />
+                                <img src="{{ asset('frontend/assets/imgs/theme/icons/icon-3.svg') }}" alt="" />
                             </div>
                             <div class="banner-text">
                                 <h3 class="icon-box-title">Great daily deal</h3>
@@ -60,7 +60,7 @@
                     <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
                         <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp" data-wow-delay=".3s">
                             <div class="banner-icon">
-                                <img src="assets/imgs/theme/icons/icon-4.svg" alt="" />
+                                <img src="{{ asset('frontend/assets/imgs/theme/icons/icon-4.svg') }}" alt="" />
                             </div>
                             <div class="banner-text">
                                 <h3 class="icon-box-title">Wide assortment</h3>
@@ -71,7 +71,7 @@
                     <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
                         <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp" data-wow-delay=".4s">
                             <div class="banner-icon">
-                                <img src="assets/imgs/theme/icons/icon-5.svg" alt="" />
+                                <img src="{{ asset('frontend/assets/imgs/theme/icons/icon-5.svg') }}" alt="" />
                             </div>
                             <div class="banner-text">
                                 <h3 class="icon-box-title">Easy returns</h3>
