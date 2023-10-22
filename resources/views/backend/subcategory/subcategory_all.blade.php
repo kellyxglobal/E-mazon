789ae1da55fa5429aeb56cc8cfba41175fc49e8d
Kelechi <kellyxglobal@gmail.com>
2023-10-22 16:32:50 +0100

    Adding, editing, updating and deleting subcategories

--- resources/views/backend/subcategory/subcategory_all.blade.php
+++ resources/views/backend/subcategory/subcategory_all.blade.php
@@ -16,7 +16,7 @@
 					</div>
 					<div class="ms-auto">
 						<div class="btn-group">
-		<a href="{{ route('add.category') }}" class="btn btn-primary">Add SubCategory</a> 				 
+		<a href="{{ route('add.subcategory') }}" class="btn btn-primary">Add SubCategory</a> 				 
 						</div>
 					</div>
 				</div>
@@ -39,12 +39,12 @@
 	@foreach($subcategories as $key => $item)		
 			<tr>
 				<td> {{ $key+1 }} </td>
-				<td> {{ $item->category_id }}</td>
+				<td> {{ $item['category']['category_name'] }}</td>
 				<td> {{ $item->subcategory_name }}  </td>
 
 				<td>
-<a href="{{ route('edit.category',$item->id) }}" class="btn btn-info">Edit</a>
-<a href="{{ route('delete.category',$item->id) }}" class="btn btn-danger" id="delete" >Delete</a>
+					<a href="{{ route('edit.subcategory',$item->id) }}" class="btn btn-info">Edit</a>
+					<a href="{{ route('delete.subcategory',$item->id) }}" class="btn btn-danger" id="delete" >Delete</a>
 
 				</td> 
 			</tr>
