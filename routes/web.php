a389cf72f084902f4b46c83308f71387b406a39d
Kelechi <kellyxglobal@gmail.com>
2023-10-28 18:39:28 +0100

    Working on product edit page

--- routes/web.php
+++ routes/web.php
@@ -150,6 +150,7 @@ Route::controller(SubCategoryController::class)->group(function(){
         Route::get('/all/product' , 'AllProduct')->name('all.product');
         Route::get('/add/product' , 'AddProduct')->name('add.product');
         Route::post('/store/product' , 'StoreProduct')->name('store.product');
+        Route::get('/edit/product/{id}' , 'EditProduct')->name('edit.product');
 
 });
 
