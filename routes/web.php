5a84dfb7b8159dd408cc03158ce3fad503247b41
Kelechi <kellyxglobal@gmail.com>
2023-11-01 20:10:55 +0100

    working on the editing, updating and deleting slider images page

--- routes/web.php
+++ routes/web.php
@@ -192,6 +192,9 @@ Route::controller(SliderController::class)->group(function(){
     Route::get('/all/slider' , 'AllSlider')->name('all.slider');
     Route::get('/add/slider' , 'AddSlider')->name('add.slider');
     Route::post('/store/slider' , 'StoreSlider')->name('store.slider');
+    Route::get('/edit/slider/{id}' , 'EditSlider')->name('edit.slider');
+    Route::post('/update/slider' , 'UpdateSlider')->name('update.slider');
+    Route::get('/delete/slider/{id}' , 'DeleteSlider')->name('delete.slider');
 
 });
 
