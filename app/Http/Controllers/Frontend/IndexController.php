667ac93c45db3336d22026e8ac92a1da03644eec
Kelechi <kellyxglobal@gmail.com>
2023-11-05 23:33:27 +0100

    Displaying Frontend Details

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -49,6 +49,22 @@ class IndexController extends Controller
 
      } // End Method 
 
+     public function VendorDetails($id){
+
+      $vendor = User::findOrFail($id);
+      $vproduct = Product::where('vendor_id',$id)->get();
+      return view('frontend.vendor.vendor_details',compact('vendor','vproduct'));
+
+   } // End Method 
+
+
+   public function VendorAll(){
+
+      $vendors = User::where('status','active')->where('role','vendor')->orderBy('id','DESC')->get();
+      return view('frontend.vendor.vendor_all',compact('vendors'));
+
+   } // End Method 
+
 
 
 }
--- resources/views/frontend/home/home_vendor_list.blade.php
+++ resources/views/frontend/home/home_vendor_list.blade.php
@@ -7,7 +7,7 @@ $vendors = App\Models\User::where('status','active')->where('role','vendor')->or
 
         <div class="section-title wow animate__animated animate__fadeIn" data-wow-delay="0">
                     <h3 class="">All Our Vendor List </h3>
-                    <a class="show-all" href="shop-grid-right.html">
+                    <a class="show-all" href="{{ route('vendor.all') }}">
                         All Vendors
                         <i class="fi-rs-angle-right"></i>
                     </a>
@@ -20,7 +20,7 @@ $vendors = App\Models\User::where('status','active')->where('role','vendor')->or
                         <div class="vendor-wrap mb-40">
                             <div class="vendor-img-action-wrap">
                                 <div class="vendor-img">
-                                    <a href="vendor-details-1.html">
+                                    <a href="{{ route('vendor.details',$vendor->id) }}">
                                         <img class="default-img" src="{{ (!empty($vendor->photo)) ? url('upload/vendor_images/'.$vendor->photo):url('upload/no_image.jpg') }}" style="width:120px;height: 120px;" alt="" />
                                     </a>
                                 </div>
@@ -34,7 +34,7 @@ $vendors = App\Models\User::where('status','active')->where('role','vendor')->or
                                         <div class="product-category">
                                             <span class="text-muted">Since {{ $vendor->vendor_join }}</span>
                                         </div>
-                                        <h4 class="mb-5"><a href="vendor-details-1.html">{{ $vendor->name }}</a></h4>
+                                        <h4 class="mb-5"><a href="{{ route('vendor.details',$vendor->id) }}">{{ $vendor->name }}</a></h4>
                                         <div class="product-rate-cover">
 
                                             @php
@@ -52,7 +52,7 @@ $vendors = App\Models\User::where('status','active')->where('role','vendor')->or
                                     <li><img src="{{ asset('frontend/assets/imgs/theme/icons/icon-contact.svg') }}" alt="" /><strong>Call Us:</strong><span>{{ $vendor->phone }}</span></li>
                                     </ul>
                                 </div>
-                                <a href="vendor-details-1.html" class="btn btn-xs">Visit Store <i class="fi-rs-arrow-small-right"></i></a>
+                                <a href="{{ route('vendor.details',$vendor->id) }}" class="btn btn-xs">Visit Store <i class="fi-rs-arrow-small-right"></i></a>
                             </div>
                         </div>
                     </div>
