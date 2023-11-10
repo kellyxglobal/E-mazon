978b4ff16f97dbcd136d734c2ed500880e8625d4
Kelechi <kellyxglobal@gmail.com>
2023-11-10 23:51:28 +0100

    Adding products to the compare box

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use App\Http\Controllers\Backend\BannerController;
 use App\Http\Controllers\Frontend\IndexController;
 use App\Http\Controllers\Frontend\CartController;
 use App\Http\Controllers\User\WishlistController;
+use App\Http\Controllers\User\CompareController;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 
@@ -239,6 +240,8 @@ Route::get('/minicart/product/remove/{rowId}', [CartController::class, 'RemoveMi
 Route::post('/dcart/data/store/{id}', [CartController::class, 'AddToCartDetails']);
 /// Add to Wishlist 
 Route::post('/add-to-wishlist/{product_id}', [WishlistController::class, 'AddToWishList']);
+/// Add to Compare 
+Route::post('/add-to-compare/{product_id}', [CompareController::class, 'AddToCompare']);
 
 /// User All Route
 Route::middleware(['auth','role:user'])->group(function() {
@@ -248,6 +251,7 @@ Route::middleware(['auth','role:user'])->group(function() {
        Route::get('/wishlist' , 'AllWishlist')->name('wishlist');
        Route::get('/get-wishlist-product' , 'GetWishlistProduct');
        Route::get('/wishlist-remove/{id}' , 'WishlistRemove');
+       
    
    
    }); 
