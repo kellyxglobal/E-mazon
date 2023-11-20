6a702874bef1c00f65ab8223efd0240c05495fd2
Kelechi <kellyxglobal@gmail.com>
2023-11-20 16:08:22 +0100

    fixed bugs in Remove Cart and Checkout page functionality

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -19,6 +19,10 @@ class CartController extends Controller
 {
     public function AddToCart(Request $request, $id){
 
+        if(Session::has('coupon')){
+            Session::forget('coupon');
+        }
+
         $product = Product::findOrFail($id);
 
         if ($product->discount_price == NULL) {
@@ -85,6 +89,10 @@ class CartController extends Controller
     }// End Method
 
     public function AddToCartDetails(Request $request, $id){
+
+        if(Session::has('coupon')){
+            Session::forget('coupon');
+        }
 
         $product = Product::findOrFail($id);
 
@@ -153,6 +161,19 @@ class CartController extends Controller
 
     public function CartRemove($rowId){
         Cart::remove($rowId);
+
+        if(Session::has('coupon')){
+            $coupon_name = Session::get('coupon')['coupon_name'];
+            $coupon = Coupon::where('coupon_name',$coupon_name)->first();
+
+           Session::put('coupon',[
+                'coupon_name' => $coupon->coupon_name, 
+                'coupon_discount' => $coupon->coupon_discount, 
+                'discount_amount' => round(Cart::total() * $coupon->coupon_discount/100), 
+                'total_amount' => round(Cart::total() - Cart::total() * $coupon->coupon_discount/100 )
+            ]); 
+        }
+
         return response()->json(['success' => 'Successfully Remove From Cart']);
 
     }// End Method
